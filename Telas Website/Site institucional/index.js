67c370d0812dfca2e33b6c6dfbe2707c41211931
GustavoSLeite <gustavo.leite@sptech.school>
2024-10-12 19:18:25 -0300

    site Atualizado

---
+++ Telas Website/Site institucional/index.js
@@ -0,0 +1,110 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.second-section-img', {
+    duration: 1000,
+    distance: '90px',
+    delay:400,
+    origin: 'right'
+
+});
+
+sr.reveal('.texto', {
+    duration: 1000,
+    distance: '90px',
+    delay:200,
+    origin: 'left'
+
+});
+
+sr.reveal('.box', {
+    duration: 2000,
+    distance: '90px'
+
+});
+
+
+
+//SEÇÃO BOXES (MISSÃO/VISÃO/VALORES)
+sr.reveal('.box1', {
+    duration: 1000,
+    distance: '90px',
+    delay: 200
+
+});
+
+sr.reveal('.box2', {
+    duration: 1000,
+    distance: '90px',
+    delay: 400
+
+});
+
+sr.reveal('.box3', {
+    duration: 1000,
+    distance: '90px',
+    delay: 600
+
+});
+
+//SEÇÃO BENEFICIOS
+sr.reveal('.beneficios', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 200
+
+});
+
+//SE~ÇÃO ATENDIMENTO
+sr.reveal('.atendimento', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 400
+
+});
+
+//SEÇÃO SERVIÇOS
+sr.reveal('.passo1', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 200
+
+});
+
+sr.reveal('.passo15', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 400
+
+});
+
+sr.reveal('.passo2', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 200
+
+});
+
+sr.reveal('.passo3', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'right',
+    delay: 400
+
+});
+
+sr.reveal('.passo4', {
+    duration: 1000,
+    distance: '90px',
+    origin: 'left',
+    delay: 200
+
+});
+
+
+
+
