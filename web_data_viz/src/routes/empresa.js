74663cdfea7c69495b232ebadf904af5287f14a3
nicollasbispo <nicollas.bpereira@sptech.school>
2024-11-20 15:36:29 -0300

    integrando cadastro e login

--- web_data_viz/src/routes/empresa.js
+++ web_data_viz/src/routes/empresa.js
@@ -1,15 +1,15 @@
 var express = require("express");
 var router = express.Router();
 
-var usuarioController = require("../controllers/usuarioController");
+var empresaController = require("../controllers/empresaController");
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+//Recebendo os dados do html e direcionando para a função cadastrar de empresaController.js
 router.post("/cadastrar", function (req, res) {
-    usuarioController.cadastrar(req, res);
+    empresaController.cadastrar(req, res);
 })
 
 router.post("/autenticar", function (req, res) {
-    usuarioController.autenticar(req, res);
+    empresaController.autenticar(req, res);
 });
 
 module.exports = router;
